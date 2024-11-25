2ae4f1ceae8b36e07ae9d8985dc1c67821dd59fb
rudrayadav <rudracom429@gmail.com>
2024-11-26 00:20:54 +0530

    about change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+  base: '/TGA.com/',
+   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
