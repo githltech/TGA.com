b7d3e59cbfc65943f02917257e02565f13d4f35a
rudrayadav <rudracom429@gmail.com>
2024-12-07 18:33:07 +0530

    today task completed

--- src/pages/termsAndConditions/TermsAndConditions.jsx
+++ src/pages/termsAndConditions/TermsAndConditions.jsx
@@ -107,7 +107,7 @@ const TermsAndConditions = () => {
 
   return (
     <Layout>
-      <div className=" py-8 px-4 md:px-16 lg:px-24 mt-28 ">
+      <div className=" py-8 px-4 md:px-16 lg:px-24 mt-10 lg:mt-28 ">
       <div className=" bg-white">
         {/* Tabs */}
         <div className="flex border-b border-t">
