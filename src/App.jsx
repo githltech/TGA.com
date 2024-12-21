9316924f9ec20a8083c5889f041f6b8eab60a158
ydvrudra <rudrapratapyadav141@gmail.com>
2024-12-21 18:34:47 +0530

    today task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Aboutus from './pages/aboutus/Aboutus';
 import Trecks_Tours from './pages/Trecks_Tours';
